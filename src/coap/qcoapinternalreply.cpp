9840e8fbbc19f69a8efb39c48d91f44ffb64663e
Adrien Leravat <aleravat@witekio.com>
2017-11-11 20:57:29 +0000

    Fix missing parentheses

--- src/coap/qcoapinternalreply.cpp
+++ src/coap/qcoapinternalreply.cpp
@@ -192,7 +192,7 @@ void QCoapInternalReply::addOption(const QCoapOption &option)
         blockNumber = (blockNumber << 4) | ((optionData[option.length()-1]) >> 4);
         d->currentBlockNumber = blockNumber;
         d->hasNextBlock = ((optionData[option.length()-1] & 0x8) == 0x8);
-        d->blockSize = static_cast<uint>(1u << (optionData[option.length()-1] & 0x7) + 4);
+        d->blockSize = static_cast<uint>(1u << ((optionData[option.length()-1] & 0x7) + 4));
     }
 
     d->message.addOption(option);
