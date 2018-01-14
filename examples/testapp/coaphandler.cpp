b7c4a081426f0883c73f3c6a4507a6ce09db2420
Adrien Leravat <aleravat@witekio.com>
2018-01-14 17:02:07 +0000

    Improve example code and add license headers

--- examples/testapp/coaphandler.cpp
+++ examples/testapp/coaphandler.cpp
@@ -1,3 +1,32 @@
+/****************************************************************************
+**
+** Copyright (C) 2017 Witekio.
+** Contact: https://witekio.com/contact/
+**
+** This file is part of the QtCoap module.
+**
+** $QT_BEGIN_LICENSE:GPL$
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The Qt Company. For licensing terms
+** and conditions see http://www.qt.io/terms-conditions. For further
+** information use the contact form at http://www.qt.io/contact-us.
+**
+** GNU General Public License Usage
+** Alternatively, this file may be used under the terms of the GNU
+** General Public License version 3 or (at your option) any later version
+** approved by the KDE Free Qt Foundation. The licenses are as published by
+** the Free Software Foundation and appearing in the file LICENSE.GPL3
+** included in the packaging of this file. Please review the following
+** information to ensure the GNU General Public License requirements will
+** be met: https://www.gnu.org/licenses/gpl-3.0.html.
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
 #include "coaphandler.h"
 
 #include <QDebug>
@@ -11,35 +40,41 @@ CoapHandler::CoapHandler(QObject *parent) : QObject(parent)
     connect(&m_coapClient, &QCoapClient::error, this, &CoapHandler::onError);
 
     QCoapReply *getReply = m_coapClient.get(QUrl("coap://172.17.0.3/test"));
-    connect(getReply, &QCoapReply::finished, this, &CoapHandler::onGetFinished);
+    connect(getReply, &QCoapReply::finished, this, &CoapHandler::onFinished);
 
     QCoapReply *observeReply = m_coapClient.observe(QUrl("coap://172.17.0.3/obs"));
     connect(observeReply, &QCoapReply::notified, this, &CoapHandler::onNotified);
+    connect(observeReply, &QCoapReply::finished, this, &CoapHandler::onFinished);
 
     QCoapDiscoveryReply *discoveryReply = m_coapClient.discover(QUrl("coap://172.17.0.3"));
     connect(discoveryReply, &QCoapDiscoveryReply::discovered, this, &CoapHandler::onDiscovered);
+    connect(discoveryReply, &QCoapReply::finished, this, &CoapHandler::onFinished);
 }
 
-void CoapHandler::onGetFinished(QCoapReply *reply)
+void CoapHandler::onFinished(QCoapReply *reply)
 {
-    if (reply->errorReceived() != QtCoap::NoError)
-        return;
+    if (reply->errorReceived() == QtCoap::NoError)
+        qDebug() << "Reply finished with payload: " << reply->readAll();
+    else
+        qDebug() << "Reply failed";
 
-    qDebug() << "Received GET answer: " << reply->readAll();
+    // Don't forget to remove the reply
+    reply->deleteLater();
 }
 
 void CoapHandler::onNotified(QCoapReply *reply, QCoapMessage message)
 {
     Q_UNUSED(message)
-    qDebug() << "Received OBSERVE notification: " << reply->readAll();
+
     // You can alternatively use `message.payload();`
+    qDebug() << "Received OBSERVE notification: " << reply->readAll();
 }
 
 void CoapHandler::onDiscovered(QCoapDiscoveryReply *reply, QVector<QCoapResource> resources)
 {
     Q_UNUSED(reply)
 
-    foreach(const QCoapResource res, resources) {
+    foreach (const QCoapResource res, resources) {
         qDebug() << "Resource discovered: " << res.path() << res.title();
     }
 }
